60d331f256ff4bc1a2049c0f2b2e31f7d32cd37c
Mert ÖZÇELİK <53866636+OzcelikMert@users.noreply.github.com>
2023-05-31 17:06:23 +0300

    yy

---
+++ js/main.js
@@ -0,0 +1,152 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Main JS
+/*
+-----------------------------------------------------------------------------------*/  
+
+(function($) {
+
+   /*---------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });     
+
+  	})
+
+   /*---------------------------------------------------- */
+	/* Final Countdown Settings
+	------------------------------------------------------ */
+	var finalDate = '2019/08/07';
+
+	$('div#counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
+
+   		$(this).html(event.strftime('<span>%D <em>Gün</em></span>' + 
+   										 	 '<span>%H <em>Saat</em></span>' + 
+   										 	 '<span>%M <em>Dakika</em></span>' +
+   										 	 '<span>%S <em>Saniye</em></span>'));
+
+   });
+
+   /*----------------------------------------------------*/
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */  	 
+	$('input').placeholder() 
+	
+
+   /*----------------------------------------------------- */
+   /* Modals
+   ------------------------------------------------------- */   
+   $('.modal-toggles ul').on('click', 'a', function(e) {
+
+   	var html = $('html'),
+   		 main = $('main, footer'),
+   		 footer = $('footer'),           
+          curMod = $(this).attr('href'),  
+          modal = $(curMod),
+          modClose = modal.find('#modal-close');          
+         
+		main.fadeOut(500, function(){
+			$('html,body').scrollTop(0);
+        	modal.addClass('is-visible');
+      });  
+      
+      e.preventDefault();
+
+      // for old ie
+      if (html.hasClass('oldie')) {
+
+      	$(document).on('click', "#modal-close", function(evt) {
+	      	$('html,body').scrollTop(0); 
+	      	modal.removeClass('is-visible');
+	      	setTimeout(function() {      
+	        		main.fadeIn(500); 
+	        	}, 500);       
+	        	        
+	        	evt.preventDefault();
+      	});
+
+      }
+      // other browsers
+      else {
+
+      	modClose.on('click', function(evt) {
+	      	$('html,body').scrollTop(0); 
+	      	modal.removeClass('is-visible');
+	      	setTimeout(function() {      
+	        		main.fadeIn(500); 
+	        	}, 500);       
+	        	        
+	        	evt.preventDefault();
+	      });
+
+      }     	
+
+   });
+
+   /*---------------------------------------------------- */
+	/* Owl Carousel
+	------------------------------------------------------ */ 
+	$("#owl-slider").owlCarousel({
+        navigation: false,
+        pagination: true,
+        items: 4,
+        navigationText: false
+    });
+
+
+   /*----------------------------------------------------*/
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function() {
+
+   	  $('main h1, #mod-about h1').fitText(1.1, { minFontSize: '28px', maxFontSize: '38px' });
+
+  	}, 100);
+
+
+   /*---------------------------------------------------- */
+   /* ajaxchimp
+	------------------------------------------------------ */
+
+	// Example MailChimp url: https://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+	var mailChimpURL = ''
+
+
+	$('#mc-form').ajaxChimp({
+
+		language: 'es',
+	   url: mailChimpURL
+
+	});
+
+	// Mailchimp translation
+	//
+	//  Defaults:
+	//	 'submit': 'Submitting...',
+	//  0: 'We have sent you a confirmation email',
+	//  1: 'Please enter a value',
+	//  2: 'An email address must contain a single @',
+	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
+	//  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+	$.ajaxChimp.translations.es = {
+	  'submit': 'Submitting...',
+	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+	}
+
+})(jQuery);
---
+++ menu2.php
@@ -0,0 +1,17 @@
+<ul id="menu" >
+    <li><a href="#">Hizmetler</a></li>
+    <li><a href="#">Ürünler</a>
+    <ul>
+        <li><a href="#">Web Site Yapımı</a></li>
+        <li><a href="#">Site Skriptleri </a></li>
+    </ul></li>
+
+    <li><a href="#">Hakkımızda</a>
+    <ul>
+    <li>
+        <li><a href="#">Hakkımızda</a></li>
+        <li><a href="#">İletişim</a></li>
+        <li><a href="#">Ekibimiz</a></li>
+    </ul>
+    </li>
+</ul>
